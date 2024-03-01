35e8c0a8e9e993ad0125b9996d75eac9238c8dd8
meriem <meriem@gmail.com>
2024-03-01 12:12:33 +0100

    display models

--- lib/constants/api_constants.dart
+++ lib/constants/api_constants.dart
@@ -1,3 +1,3 @@
 String BASE_URL = "https://api.openai.com/v1";
 
-String API_KEY = "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";
+String API_KEY = "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";
